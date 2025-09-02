b7cd874045f26962fe7f1ff5fcd4532b6681a55d
RichiHubb <ricardovaldes3477@gmail.com>
2025-09-02 18:25:03 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -83,6 +83,14 @@ app.run(["$rootScope", "$location", "$timeout", function($rootScope, $location, 
 app.controller("appCtrl", function ($scope, $http) {
 })
 app.controller("productosCtrl", function ($scope, $http) {
+    function buscarProductos() {
+        $.get("/tbodyProductos", function (trsHTML) {
+            $("#tbodyProductos").html(trsHTML)
+        })
+    }
+
+    buscarProductos()
+    
     $(document).on("submit", "#frmProducto", function (event) {
         event.preventDefault()
 
@@ -92,7 +100,7 @@ app.controller("productosCtrl", function ($scope, $http) {
             precio: $("#txtPrecio").val(),
             existencias: $("#txtExistencias").val(),
         }, function (respuesta) {
-            //
+            buscarProductos()
         })
     })
 
@@ -134,3 +142,5 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     activeMenuOption(location.hash)
 })
+
+
